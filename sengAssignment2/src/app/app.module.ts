2477b2a835c0af5997a779c3a8632a9e422d2a9c
Minh Nguyen <minh.nguyenhoangbao@ucalgary.ca>
2020-10-13 14:25:21 -0600

    test

--- sengAssignment2/src/app/app.module.ts
+++ sengAssignment2/src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { GameBoardComponent } from './board/game-board/game-board.component';
+import { GameCellComponent } from './board/game-cell/game-cell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameBoardComponent,
+    GameCellComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
